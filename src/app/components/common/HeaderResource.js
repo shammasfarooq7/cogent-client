97aaff7212803870887325c0b2adf7eea308972a
Fur619 <furqanarshad98.fa@gmail.com>
2023-05-08 02:37:05 +0500

    Fix Fontfamily

--- src/app/components/common/HeaderResource.js
+++ src/app/components/common/HeaderResource.js
@@ -4,14 +4,13 @@ import Typography from '@mui/material/Typography';
 
 
 
-export const HeaderResource = ({heading}) => {
-  
-    return (
-            <Box sx={{marginTop:"10px", marginBottom :"12px"}}>
-            <Typography id="modal-modal-description" sx={{ p:1 , background : "#EFF4FA" , borderRadius:"5px" , fontWeight:"700" , paddingLeft:"8px", fontFamily:"popins"}}>
-            {heading}
-            </Typography>
-            </Box>
-    );
-  }
-  
+export const HeaderResource = ({ heading }) => {
+
+        return (
+                <Box sx={{ marginTop: "10px", marginBottom: "12px" }}>
+                        <Typography id="modal-modal-description" sx={{ p: 1, background: "#EFF4FA", color: "#464E5F", borderRadius: "5px", fontWeight: "600", fontSize: "14px", paddingLeft: "24px", }}>
+                                {heading}
+                        </Typography>
+                </Box>
+        );
+}
